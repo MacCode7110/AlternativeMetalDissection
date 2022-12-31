537e1a5ad07c68e721ca68ddf7ad546f0c1889bc
Matthew McAlarney <memcalarney@wpi.edu>
2022-12-30 22:54:30 -0500

    Removed HTML and body tags surrounding navigation bar code on homepage

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 function homePage()
 {
     return (
-            <html lang = "en" class = "has-background-white">
-                <body>
-                <nav class = "navbar is-fixed-top has-shadow has-background-danger-light">
+            <nav class = "navbar is-fixed-top has-shadow has-background-danger-light">
                 <div class = "navbar-menu" id = "nav-links">
                     <div class = "navbar-start">
                         <a class = "navbar-item is-size-6 has-text-dark has-text-weight-bold is-family-monospace" href = "https://alternative-metal-dissection.vercel.app">
@@ -21,9 +19,7 @@ function homePage()
                     </div>
                 </div>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"></link>
-                </nav>
-                </body>
-            </html>
+            </nav>
         );   
 } 
 
