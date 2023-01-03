023af64122859b59c119bf7158e7afae26af31bd
Matthew McAlarney <memcalarney@wpi.edu>
2023-01-02 21:25:44 -0500

    Made the navigation bar of the website its own component. The navigation bar appears on each page exceot for the record request page.

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import '../styles/globals.css'
+import NavBar from '../components/Nav_Bar'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+  <NavBar/>
+  <Component {...pageProps} /> </>
 }
 
 //the app.js file is the parent React component - it is entirely responsible for rendering each of the other React components that are exported from their respective files, which are used to generate the website
 
-export default MyApp
+export default MyApp;
