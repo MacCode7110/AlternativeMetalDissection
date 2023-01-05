7b2a412a8c0b7a179844df6bd3b06665442823f1
Matthew McAlarney <memcalarney@wpi.edu>
2023-01-04 21:02:21 -0500

    Added back a thank you page when a record review request is submitted.

--- pages/record_request/record_review_request.js
+++ pages/record_request/record_review_request.js
@@ -1,24 +1,32 @@
 function recordReviewRequest()
 {
     return (
-    <form action = "https://formsubmit.co/mattmcal88@gmail.com" method = "POST" name = "Record Request Form">
-        <div class="field">
-            <label class="label"> Record Name </label>
-                <div class="control">
-                    <input class="input" type="text" placeholder="Record Name"/>
-                </div>
-        </div>
-        <div class="field">
-            <label class = "label"> Band Name </label>
-                <div class="control">
-                    <input class="input" type="text" placeholder="Band Name"/>
-                </div>
-        </div>
-        <div class="control">
-            <button class="button is-link"> Submit Request </button>
-        </div>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"></link>
-    </form>
+        <body>
+            <section class = "section is-medium">
+                <h2 class = "title">
+                    Request a Record for Review
+                </h2>
+                <form action = "https://formsubmit.co/mattmcal88@gmail.com" method = "POST" name = "Record Request Form"> <br />
+                    <input type = "hidden" name = "_next" value = "https://alternative-metal-dissection.vercel.app/record_request/thank_you.js"/>
+                    <div class="field">
+                        <label class="label is-medium"> Record Name </label>
+                            <div class="control">
+                                <input class="input is-medium is-success" type="text" placeholder="Record Name"/> <br />
+                            </div>
+                    </div>
+                    <div class="field">
+                        <label class = "label is-medium"> Band Name </label>
+                            <div class="control">
+                                <input class="input is-medium is-success" type="text" placeholder="Band Name"/> <br />
+                            </div>
+                    </div>
+                    <div class="control">
+                        <button class="button is-link is-medium is-success"> Submit Request </button>
+                    </div>
+                </form>
+            </section>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"></link>
+        </body>
     );
 }
 
---
+++ pages/record_request/thank_you.js
@@ -0,0 +1,15 @@
+function thankYou()
+{
+    return (
+        <body>
+            <section class = "section is-medium">
+                <h2 class = "title">
+                    Thank you for requesting a record for review!
+                </h2>
+            </section>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"></link>
+        </body>
+    );
+}
+
+export default thankYou;
