7806888eec0e1a9d14d78bec3fa6119d28420c06
Matthew McAlarney <memcalarney@wpi.edu>
2023-01-07 17:14:08 -0500

    Added form validation for the record review request page.

--- pages/record_review_request/record_review_request.js
+++ pages/record_review_request/record_review_request.js
@@ -2,26 +2,28 @@ function recordReviewRequest()
 {
     return (
         <body>
+            <script defer src = "record_review_request_form_validation.js"></script>
+            <div id = "error"></div>
             <section class = "section is-medium">
                 <h1 class = "title has-text-weight-bold">
                     Request a Record for Review
                 </h1>
-                <form action = "https://formsubmit.co/mattmcal88@gmail.com" method = "POST" name = "Record Request Form"> <br />
+                <form id = "form" action = "https://formsubmit.co/mattmcal88@gmail.com" method = "POST" name = "Record Request Form"> <br />
                     <input type = "hidden" name = "_next" value = "https://alternative-metal-dissection.vercel.app/record_review_request/thank_you"/>
                     <div class="field">
                         <label class="label is-size-4 has-text-weight-normal"> Record Name </label>
                             <div class="control">
-                                <input class="input is-medium is-success" type="text" placeholder="Record Name" name = "Record Name"/> <br />
+                                <input class="input is-medium is-success" id = "recordname" type="text" placeholder="Record Name" name = "Record Name"/> <br />
                             </div>
                     </div>
                     <div class="field">
                         <label class = "label is-size-4 has-text-weight-normal"> Band Name </label>
                             <div class="control">
-                                <input class="input is-medium is-success" type="text" placeholder="Band Name" name = "Band Name"/> <br />
+                                <input class="input is-medium is-success" id = "bandname" type="text" placeholder="Band Name" name = "Band Name"/> <br />
                             </div>
                     </div>
                     <div class="control">
-                        <button class="button is-link is-size-4 has-text-weight-normal is-success"> Submit Request </button>
+                        <button type = "submit" class="button is-link is-size-4 has-text-weight-normal is-success"> Submit Request </button>
                     </div>
                 </form>
             </section>
