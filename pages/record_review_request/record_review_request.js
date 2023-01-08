dbf39af8ac21e78d5f063dbf193eef2c6e5c062b
Matthew McAlarney <memcalarney@wpi.edu>
2023-01-08 18:45:28 -0500

    Rewrote form validation using alert() method instead of pushing error messages to a list.

--- pages/record_review_request/record_review_request.js
+++ pages/record_review_request/record_review_request.js
@@ -2,22 +2,20 @@ function recordReviewRequest()
 {
     return (
         <body>
-            <script defer src = "record_review_request_form_validation.js"></script>
-            <div id = "error"></div>
             <section class = "section is-medium">
                 <h1 class = "title has-text-weight-bold">
                     Request a Record for Review
                 </h1>
-                <form id = "form" action = "https://formsubmit.co/mattmcal88@gmail.com" method = "POST" name = "Record Request Form"> <br />
+                <form onsubmit = "validateRecordRequestReviewForm()" action = "https://formsubmit.co/mattmcal88@gmail.com" method = "POST" name = "Record Request Form"> <br />
                     <input type = "hidden" name = "_next" value = "https://alternative-metal-dissection.vercel.app/record_review_request/thank_you"/>
                     <div class="field">
-                        <label class="label is-size-4 has-text-weight-normal"> Record Name </label>
+                        <label for = "recordname" class="label is-size-4 has-text-weight-normal"> Record Name </label>
                             <div class="control">
                                 <input class="input is-medium is-success" id = "recordname" type="text" placeholder="Record Name" name = "Record Name"/> <br />
                             </div>
                     </div>
                     <div class="field">
-                        <label class = "label is-size-4 has-text-weight-normal"> Band Name </label>
+                        <label for = "bandname" class = "label is-size-4 has-text-weight-normal"> Band Name </label>
                             <div class="control">
                                 <input class="input is-medium is-success" id = "bandname" type="text" placeholder="Band Name" name = "Band Name"/> <br />
                             </div>
@@ -33,3 +31,32 @@ function recordReviewRequest()
 }
 
 export default recordReviewRequest;
+
+function validateRecordRequestReviewForm() {
+    const recordName = document.querySelector('#recordname').value;
+    const bandName = document.querySelector('#bandname').value;
+
+    if(recordName === '' || recordName == null) { //=== compares both the data types and values of the operands, while == performs a data type conversion BEFORE the comparison of the values of the operands.
+        alert('Record Name is a required field');
+        return false;
+    }
+
+    if(bandName === '' || recordName == null) {
+        alert('Band Name is a required field');
+        return false;
+    }
+
+    if(recordName.length > 125)
+    {
+        alert('Record Name must be no longer than 125 characters');
+        return false;
+    }
+
+    if(bandName.length > 125)
+    {
+        alert('Band Name must be no longer than 125 characters');
+        return false;
+    }
+}
+
+//For attribute is used in labels, and it establishes (gives the label) an id for the associated input.
