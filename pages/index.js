3c4f16427420eae195d645193eaf63ba2b6073e5
Matthew McAlarney <memcalarney@wpi.edu>
2023-05-09 18:53:36 -0400

    Added a card component to the homepage with an embedded link to the first opinion article.

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,21 @@
+import {getOpinionArticleByID} from '../opinion_data'
+
 function homePage()
 {
+    const {id, title} = getOpinionArticleByID('o1');
+    const opinionArticleLink = `../../opinion_articles/${id}`;
+
     return (<div>
         <section className = "section is-medium">
-            <div className = "box has-background-light">
-                <article className = "media">
-                    <div className = "media-content">
-                        <div className = "content">
-                            <p> test </p>
-                        </div>
-                    </div>
-                </article>
+            <div className = "card has-background-light">
+                <div className = "card-content">
+                    <p className = "title is-1 is-spaced">
+                        Placeholder
+                    </p>
+                    <p className = "subtitle is-3">
+                        Read <a href = {opinionArticleLink}> {title} </a>
+                    </p>
+                </div>
             </div>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"></link>
         </section>
